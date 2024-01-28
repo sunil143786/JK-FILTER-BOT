cbba3b39cc6af1f7893fed1cb25a35109ec076c1
sunil143786 <141899550+sunil143786@users.noreply.github.com>
2024-01-28 13:15:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,8 +21,7 @@ TIMEZONE = environ.get("TIMEZONE", "Asia/Kolkata")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = is_enabled((environ.get('USE_CAPTION_FILTER', 'True')), True)
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/93b797b068afe6ca96a31.jpg
-https://telegra.ph/file/72a2b4edec6d0a932150e.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/93b797b068afe6ca96a31.jpg   https://telegra.ph/file/72a2b4edec6d0a932150e.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
